d95fdf8a577f91404e7c5ebd29445da2f2bcf889
jimmy0524 <10jmin04@naver.com>
2025-01-29 05:28:50 +0900

    도메인 분석 설계

---
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@DiscriminatorValue("A")
+public class Album extends Item{
+}
---
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@DiscriminatorValue("M")
+public class Movie extends Item {
+}
