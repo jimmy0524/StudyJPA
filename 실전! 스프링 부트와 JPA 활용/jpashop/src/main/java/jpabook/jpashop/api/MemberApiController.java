f959dbf80fa5a621882623bdb5e104c68783436a
jimmy0524 <10jmin04@naver.com>
2025-02-04 01:04:28 +0900

    회원 api 개발

---
+++ 실전! 스프링 부트와 JPA 활용/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,102 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    //v1: 엔티티를 Request Body에 직접 매핑
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    //v2: 엔티티 대신에 DTO를 RequestBody에 매핑
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid
+                                             CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    //v1: 응답 값으로 엔티티를 직접 외부에 노출
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    //v2: 응답 값으로 엔티티가 아닌 별도의 DTO 사용
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @AllArgsConstructor
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @AllArgsConstructor
+    @Data
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @AllArgsConstructor
+    @Data
+    static class Result<T> {
+        private T data;
+    }
+
+    @AllArgsConstructor
+    @Data
+    static class MemberDto {
+        private String name;
+    }
+}
