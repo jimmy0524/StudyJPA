fbbec94d9c45ea0e89e802177375df813c699911
jimmy0524 <10jmin04@naver.com>
2025-03-09 16:02:17 +0900

    Specifications

--- 실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -4,14 +4,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
+import study.data_jpa.entity.Team;
+import study.data_jpa.specification.MemberSpec;
 
 @SpringBootTest
 @Transactional
@@ -127,8 +131,29 @@ public class MemberRepositoryTest {
         //when
         Member findMember = memberRepository.findById(member.getId()).get();
 
+//        //then
+//        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+//        System.out.println("findMember.updatedDate = " + findMember.getUpdatedDate());
+    }
+
+    @Test
+    public void specBasic() throws Exception {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+        em.flush();
+        em.clear();
+
+        //when
+        Specification<Member> spec = MemberSpec.username("m1").and(MemberSpec.teamName("teamA"));
+        List<Member> result = memberRepository.findAll(spec);
+
         //then
-        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
-        System.out.println("findMember.updatedDate = " + findMember.getUpdatedDate());
+        Assertions.assertThat(result.size()).isEqualTo(1);
     }
 }
